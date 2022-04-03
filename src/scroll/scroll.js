2aa4e45065c307b3a1a9ef2c93a641bc6095e17b
thaolethac <91691058+thaolethac@users.noreply.github.com>
2022-04-03 23:15:47 +0700

    Update scroll.js

--- src/scroll/scroll.js
+++ src/scroll/scroll.js
@@ -1,13 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './scroll.css';
 // import Images from '../Header/image';
 
 const Scroll = () => {
-    
+  const Click = () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+  }
   return (
     <>
         <div className='scroll'>
-            <button>
+            <button onClick={Click}>
              <i class='fas fa-angle-double-up'></i>
             </button>
         </div>
